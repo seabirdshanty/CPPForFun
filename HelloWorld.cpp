d2bb088dbdb39aceff36c0d81e4cd8349d73ac5e
Jamie <seabirdshanty@users.noreply.github.com>
2017-03-14 19:35:37 -0700

    Hello!

---
+++ HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	string kimiNoNawa;
+	string pawse;
+	int uage;
+	cout << endl;
+	cout << " Hello World! " << endl; 
+	cout << " What is your name? Please type it in here: ";
+	cin >> kimiNoNawa;
+	cout << " Its nice to meet you, " << kimiNoNawa << "!" << endl;
+	cout << " May I also ask for your age? Please enter it here: ";
+	cin >> uage;
+	cout << " Amazing, you've lived for " << uage << " years? I've only been manufactured for 7!\n" << endl;
+	cout << " Thank you for talking to me, " << kimiNoNawa << ".\n I hope you have a wonderful day today." << endl;
+	cout << " Remember, not every day will be great, but tomorrow will always be better.\n" << endl;
+  cin >> pawse;
+	return 0;
+}
