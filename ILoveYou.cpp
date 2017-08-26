ca5529fa2cc8cb7a1dbeec4dd3d11beaafc84226
Jamie <seabirdshanty@users.noreply.github.com>
2017-08-25 22:07:44 -0700

    Update ILoveYou.cpp

--- ILoveYou.cpp
+++ ILoveYou.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	string kimiNoNawa;
+	string pawse;
+	cout << endl;
+	cout << " What is your name? Please type it in here: ";
+	cin >> kimiNoNawa;
+	cout << " Hello, " << kimiNoNawa << "! I Love You!" << endl;
+	cin >> pawse;
+	return 0;
+}
 
